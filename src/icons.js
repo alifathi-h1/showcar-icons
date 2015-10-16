cdd9c9d65f280f727b10a85f45defb4a9e68baef
unknown <sspringer@DDEVVMV011.dev.as24.local>
2015-10-16 14:19:50 +0200

    initial stuff

---
+++ src/icons.js
@@ -0,0 +1,23 @@
+var iconNames = [
+    'info'
+];
+
+var icons = {};
+
+iconNames.forEach(function(name) {
+    icons[name.toLowerCase()] = require('../icons/' + name + '.svg');
+});
+
+var proto = Object.create(HTMLElement.prototype);
+
+proto.createdCallback = function() {
+    this.innerHTML = icons[('' + this.getAttribute('type')).toLowerCase()];
+};
+
+proto.attributeChangedCallback = function(attributeName, previousValue, value) {
+    if (attributeName === 'type') {
+        this.innerHTML = icons[('' + this.getAttribute('type')).toLowerCase()];
+    }
+};
+
+document.registerElement('x-icon', { prototype: proto });
