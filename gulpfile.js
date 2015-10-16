6eb679e4283d250b804f45e33e8d26ad593a068d
unknown <sspringer@DDEVVMV011.dev.as24.local>
2015-10-16 14:39:49 +0200

    automatic generation of icon files array

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,27 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var webpack = require("webpack");
 var argv = require('yargs').argv;
+var glob = require('glob');
 var watch = argv.w;
 
 var config = Object.create(require('./webpack.config.js')(watch));
 
-gulp.task('js', function(callback) {
+gulp.task('prepare', function (callback) {
+    glob('icons/*.svg', function (err, testFiles) {
+        require('fs').readFile('src/icons.tpl.js', 'utf-8', function (err, data) {
+            var files = [];
+
+            testFiles.forEach(function (file) {
+                files.push(file.substring(file.lastIndexOf('/') + 1, file.lastIndexOf('.svg')));
+            });
+
+            var newContent = data.replace('xxx', "'" + files.join("',\n    '") + "'");
+            require('fs').writeFile('src/icons.js', newContent, callback);
+
+        })
+    });
+});
+gulp.task('js', ['prepare'], function(callback) {
     webpack(config, function(err, stats) {
         gutil.log("[webpack]", stats.toString({ chunks: false, version: false, hash: false }));
     });
--- src/icons.tpl.js
+++ src/icons.tpl.js
@@ -1,5 +1,5 @@
 var iconNames = [
-    'info'
+    xxx
 ];
 
 var icons = {};
