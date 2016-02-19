bbfcb399bc3e324efc86f288325fac20fc693039
m-idler <marcel.idler@mayflower.de>
2016-02-19 16:18:26 +0100

    fix code to register custom element only once

--- src/icons.tpl.js
+++ src/icons.tpl.js
@@ -1,37 +1,31 @@
-var isRegistered = function(name) {
-    var registered = document.createElement(name).__proto__ !== HTMLElement.prototype;
+var iconNames = [
+    '@@iconNames'
+];
 
-    if (registered && window && window.console) {
-        window.console.warn('CustomElement "' + name + '" is already registered.');
-    }
+var icons = {};
 
-    return registered;
+iconNames.forEach(function(name) {
+    icons[name.toLowerCase()] = require('../icons/' + name + '.svg');
+});
+
+var proto = Object.create(HTMLElement.prototype);
+
+proto.createdCallback = function() {
+    this.innerHTML = icons[('' + this.getAttribute('type')).toLowerCase()];
 };
 
-if (!isRegistered('as24-icon')) {
-    var iconNames = [
-        '@@iconNames'
-    ];
-
-    var icons = {};
-
-    iconNames.forEach(function(name) {
-        icons[name.toLowerCase()] = require('../icons/' + name + '.svg');
-    });
-
-    var proto = Object.create(HTMLElement.prototype);
-
-    proto.createdCallback = function() {
+proto.attributeChangedCallback = function(attributeName, previousValue, value) {
+    if (attributeName === 'type') {
         this.innerHTML = icons[('' + this.getAttribute('type')).toLowerCase()];
-    };
+    }
+};
 
-    proto.attributeChangedCallback = function(attributeName, previousValue, value) {
-        if (attributeName === 'type') {
-            this.innerHTML = icons[('' + this.getAttribute('type')).toLowerCase()];
-        }
-    };
-
+try {
     document.registerElement('as24-icon', { prototype: proto });
-
-    window.showcarIconNames = iconNames;
+} catch (e) {
+    if (window && window.console) {
+        window.console.warn('Failed to register CustomElement "as24-icon".', e);
+    }
 }
+
+window.showcarIconNames = iconNames;
