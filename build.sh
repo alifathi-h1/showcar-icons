322b36d76df7094663701b90b93107de7f5ee997
Yifan Yu <yifan.9916@gmail.com>
2018-10-17 16:14:41 +0200

    update path to dist file

--- build.sh
+++ build.sh
@@ -11,15 +11,15 @@ fail() {
 
 prepare_assets() {
     sed -i -e "s=@@ASSET_PATH=${ASSET_PATH}=" dist/index.html
-    sed -i -e "s=index.min.js.map=${ASSET_PATH}/index.min.js.map=" dist/index.min.js
+    sed -i -e "s=showcar-icons.min.js.map=${ASSET_PATH}/showcar-icons.min.js.map=" dist/showcar-icons.min.js
 }
 
 upload_to_s3() {
     echo "Uploading assets to S3"
 
-    # aws --region "eu-west-1" s3 cp dist "s3://as24-assets-eu-west-1/showcar-tracking/test/" --recursive --exclude "*.html" --cache-control "max-age=2592000" --acl public-read
+    # aws --region "eu-west-1" s3 cp dist "s3://as24-assets-eu-west-1/showcar-icons/test/" --recursive --exclude "*.html" --cache-control "max-age=2592000" --acl public-read
 
-    # aws --region "eu-west-1" s3 cp dist "s3://as24-assets-eu-west-1/showcar-tracking/test/" --recursive --exclude "*" --include "*.html" --cache-control "max-age=300" --acl public-read
+    # aws --region "eu-west-1" s3 cp dist "s3://as24-assets-eu-west-1/showcar-icons/test/" --recursive --exclude "*" --include "*.html" --cache-control "max-age=300" --acl public-read
 
     # aws --region "eu-west-1" s3 cp dist "s3://as24-assets-eu-west-1/${SERVICE}/${BRANCH}/latest/" --recursive --exclude "*.html" --cache-control "max-age=2592000" --acl public-read
 
