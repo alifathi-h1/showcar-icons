2ce81da32fb4e1174b90e238cb3e2f0b92cb857a
Adam Beres-Deak <adam.beres-deak@scout24.com>
2019-09-27 15:30:34 +0200

    Replaced arrow function with old-school `function`

--- src/icons.js
+++ src/icons.js
@@ -2,7 +2,9 @@ var icons = {};
 var regex = /^\.\/([^\.]+)\.svg$/gm;
 
 function importAll(r) {
-  r.keys().forEach(key => (icons[key.toLowerCase().replace(regex, "$1")] = r(key)));
+  r.keys().forEach(function(key) {
+    icons[key.toLowerCase().replace(regex, "$1")] = r(key); 
+  });
 }
 
 importAll(require.context("../icons", true, /\.svg$/));
